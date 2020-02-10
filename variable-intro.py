c470925b42f118439d1c4842c3c37fdbbfdb7975
padams73 <pad@stac.school.nz>
2020-02-11 10:49:05 +1300

    Variables

--- variable-intro.py
+++ variable-intro.py
@@ -1,19 +1,35 @@
-""" This program demonstrates how to use variables to store information
-and how to print out the value of a variable. """
+""" This program creates a mad lib theatre scenario """
 
-# variables should have appropriate names
-name = "John"
-# if there are multiple words in the variable name, use underscores
-last_name = "Smith"
+print("This is a mad lib program")
 
-print("Hello", name, last_name)
+name = input("Enter a name")
 
-age = 15
-print("You are", age, "years old")
+animal = input("Enter an animal")
 
-# when getting input, use the input() function
-# we need to set a variable to store the input
-fav_animal = input("What is your favourite animal?")
+number = input("Enter 16 random digits that coincide with those on your credit/debit card")
 
-print("Wrong!", fav_animal, "is nothing compared to the majestic reinturtle")
+animal_2 = input("Enter another animal")
 
+number_2 = input("Enter a date that your you credit/debit card will expire on")
+
+nickname = input("Enter a nickname")
+
+food = input("Enter a type of food")
+
+number_3 = input("Enter the three digit number on the back of your credit/debit card")
+
+name_2 = input("Enter another name")
+
+print("Thinking... \n        ... \n        ... \n Here is your Mab Lib Theatre")
+
+print("There once was a", animal, "it was called", name, "its favourite food in the world was", food) 
+print("but", nickname, "who was a", animal_2, "charged too much for the", food, "this meant that", name)
+print("had to find a way to pay for", food, "however", name, "had been taught how to use python by", name_2)
+print("who was also a", animal_2, "so he decided to make a code that stole peoples credit card details for example your credit/debit card number is")
+print(number)
+print("your expiry date is")
+print(number_2)
+print("and your CV number is")
+print(number_3)
+print("And", name+"'s", "code always finished with")
+print("You are a loser i have your money")
