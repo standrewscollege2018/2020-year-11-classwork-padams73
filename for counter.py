80efffa8bc446cf82a57166b13da602d4bd92b1f
padams73 <pad@stac.school.nz>
2020-02-13 11:47:58 +1300

    For loops

---
+++ for counter.py
@@ -0,0 +1,14 @@
+""" This program gets two numbers from the user, then counts up from the lower to the higher. """
+
+number_1 = int(input("Enter a number"))
+number_2 = int(input("Enter another number"))
+
+if number_1 > number_2:
+    start = number_2
+    stop = number_1
+else:
+    start = number_1
+    stop = number_2
+
+for i in range(start, stop+1):
+    print(i)
---
+++ forloop.py
@@ -0,0 +1,5 @@
+""" Demonstrating for loops. """
+
+# in for loops we set a start, ending and increment value
+for i in range(18, 23, 1):
+    print(i)
