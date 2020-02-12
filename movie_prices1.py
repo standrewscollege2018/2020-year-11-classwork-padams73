bdb6c660a8eb931e2a471e2858136f3010d2231a
padams73 <pad@stac.school.nz>
2020-02-13 11:47:51 +1300

    Movie prices
    Demonstrating nested if statements

--- movie_prices1.py
+++ movie_prices1.py
@@ -1,20 +1,14 @@
-""" This program calculates how much a movie ticket is for you. However, it has some mistakes and inefficiencies.
-Can you spot them? """
+age = int(input("Age"))
+if age > 13:
+    student = input("Student? (y/n)")
+    if student == "y":
+        print("$8")
+    elif age >= 18:
+        print("$12")
+    else:
+        print("$9")
+elif age >= 5:
+    print("$7")
+else:
+    print("free")
 
-#are they a student
-student_status = input("Are you a student y/n")
-
-#get the user age
-user_age = int(input("What is your age"))
-
-#give them their price
-if student_status == "y":
-    print("Ticket price is $8")
-elif user_age < 5:
-    print("You need not pay")
-elif user_age >= 5:
-    print("the price is $7")
-elif user_age >= 13:
-    print("The price is $9")
-elif user_age >= 18:
-    print("The price is $12")
