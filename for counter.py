7997b88f3481cb5dce9af0d9f369817bc82dced1
padams73 <pad@stac.school.nz>
2020-02-19 11:17:20 +1300

    For loop with variables

--- for counter.py
+++ for counter.py
@@ -3,12 +3,14 @@
 number_1 = int(input("Enter a number"))
 number_2 = int(input("Enter another number"))
 
-if number_1 > number_2:
-    start = number_2
-    stop = number_1
-else:
+if number_1 < number_2:
     start = number_1
     stop = number_2
+else:
+    start = number_2
+    stop = number_1
 
 for i in range(start, stop+1):
     print(i)
+
+print("All done")
